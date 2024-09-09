a57b65c87523b2bb5b91e052842e8cfa567222f0
abhishekbhakat <abhishek.bhakat@hotmail.com>
2024-09-09 16:48:15 +0000

    embeddings and storage context

--- fastrag/services/embeddings.py
+++ fastrag/services/embeddings.py
@@ -1,5 +1,29 @@
-from llama_index.embeddings.openai import OpenAIEmbedding
-from fastrag.config import settings
+from typing import Any
 
-def get_embed_model():
-    return OpenAIEmbedding(model=settings.EMBED_MODEL)
+from llama_index.embeddings.ollama import OllamaEmbedding
+from llama_index.embeddings.openai import OpenAIEmbedding
+
+
+def get_openai_embedding(config: dict[str, Any]) -> OpenAIEmbedding:
+    return OpenAIEmbedding(api_key=config["embedding_api_key"], api_base=config.get("embedding_api_base"), model_name=config["embedding_model"])
+
+
+
+def get_ollama_embedding(config: dict[str, Any]) -> OllamaEmbedding:
+    return OllamaEmbedding(model_name=config["embedding_model"])
+
+
+def get_lmstudio_embedding(config: dict[str, Any]) -> OpenAIEmbedding:
+    return OpenAIEmbedding(api_key=config["embedding_api_key"], api_base=config.get("embedding_api_base"), model_name=config["embedding_model"])
+
+
+def get_embedding_model(config: dict[str, Any]) -> OpenAIEmbedding | OllamaEmbedding:
+    embedding_provider = config["embeddings_provider"]
+    if embedding_provider == "openai":
+        return get_openai_embedding(config)
+    elif embedding_provider == "ollama":
+        return get_ollama_embedding(config)
+    elif embedding_provider == "lmstudio":
+        return get_lmstudio_embedding(config)
+    else:
+        raise ValueError(f"Unsupported embeddings provider: {embedding_provider}")
--- fastrag/services/storage_context.py
+++ fastrag/services/storage_context.py
@@ -1,15 +1,48 @@
-from typing import Any, Tuple
-from llama_index.core import StorageContext, VectorStoreIndex
+from typing import Any
+
+from llama_index.core import Document, StorageContext, VectorStoreIndex, load_index_from_storage
+from llama_index.core.storage.docstore import SimpleDocumentStore
+from llama_index.core.storage.index_store import SimpleIndexStore
 from llama_index.vector_stores.postgres import PGVectorStore
 
-def get_index(config: dict[str, Any], vector_store: PGVectorStore, cache_dir: str | None = None) -> Tuple[VectorStoreIndex, StorageContext]:
-    # TODO: Implement the logic to get or create the index
-    # This is a placeholder implementation
-    storage_context = StorageContext.from_defaults(vector_store=vector_store)
-    index = VectorStoreIndex([], storage_context=storage_context)
-    return index, storage_context
+from fastrag.services.embeddings import get_embedding_model
+
 
 def set_storage_context(vector_store: PGVectorStore, cache_dir: str | None = None) -> StorageContext:
-    # TODO: Implement the logic to set the storage context
-    # This is a placeholder implementation
-    return StorageContext.from_defaults(vector_store=vector_store)
+    docstore = SimpleDocumentStore()
+    index_store = SimpleIndexStore()
+    storage_context = StorageContext.from_defaults(docstore=docstore, index_store=index_store, vector_store=vector_store)
+    if cache_dir:
+        storage_context.persist(persist_dir=cache_dir)
+    return storage_context
+
+
+def persist_storage_context(storage_context: StorageContext, cache_dir: str | None = None):
+    if cache_dir:
+        storage_context.persist(persist_dir=cache_dir)
+
+
+def get_storage_context(vector_store: PGVectorStore, cache_dir: str | None = None) -> StorageContext:
+    if cache_dir:
+        storage_context = StorageContext.from_defaults(persist_dir=cache_dir, vector_store=vector_store)
+    else:
+        storage_context = set_storage_context(vector_store, cache_dir)
+    return storage_context
+
+
+def create_index(config: dict[str, Any], documents: list[Document], vector_store: PGVectorStore, cache_dir: str | None = None) -> VectorStoreIndex:
+    # TODO: Instead of config it should expect embedding model and vector store
+    embed_model = get_embedding_model(config=config)
+    storage_context = get_storage_context(vector_store, cache_dir)
+    index = VectorStoreIndex.from_documents(documents, storage_context=storage_context, embed_model=embed_model, embed_batch_size=10, index_batch_size=100)
+    persist_storage_context(storage_context, cache_dir)
+    return index
+
+
+def get_index(config: dict[str, Any], vector_store: PGVectorStore, cache_dir: str | None = None) -> tuple[VectorStoreIndex, StorageContext]:
+    if cache_dir:
+        storage_context = get_storage_context(vector_store=vector_store, cache_dir=cache_dir)
+        index = load_index_from_storage(storage_context, embed_model=get_embedding_model(config=config))
+        return index, storage_context
+    else:
+        return None, None
