5e49786a1929a135e61c993ffcf8a6d7f67d363f
abhishekbhakat <abhishek.bhakat@hotmail.com>
2024-09-23 07:55:10 +0000

    load existing storage context

--- fastrag/services/bootstrap.py
+++ fastrag/services/bootstrap.py
@@ -1,7 +1,7 @@
 import os
 
 from fastrag.config import config, logger
-from fastrag.services.storage_context import set_storage_context
+from fastrag.services.storage_context import create_index, get_index, set_storage_context
 from fastrag.services.vector_store import get_vector_store
 
 
@@ -15,8 +15,15 @@ def bootstrap_app():
     # Initialize vector store
     vs = get_vector_store(config)
 
-    # Initialize storage context and index
+    # Attempt to load the existing index
+    index, _ = get_index(config=config, vector_store=vs, cache_dir=config["cache_dir"])
+    if index:
+        logger.info("Existing index loaded successfully")
+        return
+
+    # Initialize storage context and create a new index if the existing one cannot be loaded
     _ = set_storage_context(vs, config["cache_dir"])
+    index = create_index(config=config, documents=[], vector_store=vs, cache_dir=config["cache_dir"])
 
     logger.info("Application bootstrapped successfully")
     return
